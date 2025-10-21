83167b5b786f06f7eaec839961c1e3db41dd344c
unknown <CETECOM@AOAWSB02LC0416.duoc.acad>
2025-10-21 11:41:56 -0300

    Nosotros, Blog, Dato1 y Dato2

---
+++ pages/NavBar.jsx
@@ -0,0 +1,75 @@
+import '../src/css/styles.css'
+import '../src/css/visual.css'
+import { Navbar, Nav, Container } from "react-bootstrap";
+
+function NavBar() {
+  return (
+    <Navbar expand="lg">
+      <Container>
+        <Navbar.Brand href="#">
+          <img src="img/logo-blanco.png" alt="Huerto Hogar" height="60" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarNav" />
+        <Navbar.Collapse id="navbarNav" className="justify-content-end">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div
+            className="collapse navbar-collapse justify-content-end"
+            id="navbarNav"
+          >
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="/">
+                Inicio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="productos.html">
+                Productos
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="nosotros">
+                Nosotros
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="blog">
+                Blog
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="contacto.html">
+                Contacto
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="login.html">
+                Login
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="registro.html">
+                Registrarse
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="carrito.html">
+                🛒
+              </a>
+            </li>
+          </ul>
+        </div>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavBar;
---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./css/styles.css";
+import "./css/visual.css";
+
+import Home from "../pages/Home";
+import NavBar from "../pages/NavBar";
+import Footer from "../pages/Footer";
+import Blog from "../pages/Blog";
+import Dato1 from "../pages/Dato1"; 
+import Dato2 from "../pages/Dato2"; 
+import Nosotros from "../pages/Nosotros"; 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/dato1" element={<Dato1 />} />
+          <Route path="/blog/dato2" element={<Dato2 />} />
+          <Route path="/nosotros" element={<Nosotros />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
